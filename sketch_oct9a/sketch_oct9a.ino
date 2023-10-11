0bccf9d12aecb1e83d2f75094fe6398158d59658
Fede <federica.guiducci3@studio.unibo.it>
2023-10-11 14:47:22 +0200

    only two buttons turn off

--- sketch_oct9a/sketch_oct9a.ino
+++ sketch_oct9a/sketch_oct9a.ino
@@ -1,17 +1,20 @@
-#define LEDG_PIN13 13
-#define LEDG_PIN12 12
-#define LEDG_PIN11 11
-#define LEDG_PIN10 10
+#define LEDG_PIN13 13 //button 5
+#define LEDG_PIN12 12 //button 4
+#define LEDG_PIN11 11 //button 3
+#define LEDG_PIN10 10 //button 2
 #define LEDr_PIN9 9
 #define POT_PIN A0
 #define BUTTON_PIN2 2
-#define BUTTON_PIN4 3
-#define BUTTON_PIN5 4
-#define BUTTON_PIN6 5
+#define BUTTON_PIN3 3
+#define BUTTON_PIN4 4
+#define BUTTON_PIN5 5
+
+
 
 //variabili per i tempi
 int t1 = 2000; //millisecondi prima di iniziare il gioco
-int t3= 5000; //millisecondi prima di spegnere il led dopo
+int t3= 1000; //millisecondi prima di spegnere il led dopo
+volatile int clac; //bottone premuto
 
 void setup() {
   // put your setup code here, to run once:
@@ -22,21 +25,26 @@ void setup() {
   pinMode(LEDr_PIN9, OUTPUT);
 
   pinMode(BUTTON_PIN2, INPUT);
+  pinMode(BUTTON_PIN3, INPUT);
   pinMode(BUTTON_PIN4, INPUT);
   pinMode(BUTTON_PIN5, INPUT);
-  pinMode(BUTTON_PIN6, INPUT);
 
   Serial.begin(9600);
 
-   // Inizializza la generazione dei numeri casuali
+  // Inizializza la generazione dei numeri casuali
   randomSeed(analogRead(0));
+
+  attachInterrupt(digitalPinToInterrupt(BUTTON_PIN2), clic2, RISING);
+  attachInterrupt(digitalPinToInterrupt(BUTTON_PIN3), clic3, RISING);
+  attachInterrupt(digitalPinToInterrupt(BUTTON_PIN4), clic4, RISING);
+  attachInterrupt(digitalPinToInterrupt(BUTTON_PIN5), clic5, RISING);
   
 
 
 }
 
 void loop() {
-
+  
   
   // put your main code here, to run repeatedly:
   Serial.println("------------------------");
@@ -119,33 +127,43 @@ void loop() {
 
   zero = zero /10;
   
-
+  noInterrupts();
   //inizio clic bottone utente
   int clic=0; //questa variabile contiene il numero del bottone che deve essere cliccato
   Serial.println("ordine numeri usciti");
   Serial.println(salvo);
-  int clac; //numero premuto
-  int buttonstate= digitalRead(BUTTON_PIN2);
-  clic = salvo/zero;
-
-  if(buttonstate== true){
-    Serial.println("bottone premuto");
-    digitalWrite(LEDr_PIN9, HIGH);
-  }
-
-  Serial.println("\n primo numero che premo");
-  Serial.println(clic);
+  
+  //clic = salvo/zero;
+  //salvo= salvo%zero;
 
   
 
+  Serial.println("\n primo numero che premo");
+  Serial.println(clic);
+  
+  
 
+  
   //se il clic del bottone corrisponde a quello giusto allora vai avanti (ciclo che esci quando sbagli o quando finisci)
+  //while(clic==clac){
+    
+  //}
   //se clic sbagliato esci e fai ricominciare il gioco
 
-
+  interrupts();
 
   delay(1000000000000000000000);
   
 
 
+}
+
+
+void clic4(){
+  clac=4;
+  digitalWrite(LEDG_PIN12, HIGH);
+}
+void clic5(){
+  clac=5;
+  digitalWrite(LEDG_PIN13, HIGH);
 }
